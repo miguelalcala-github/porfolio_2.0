9a571e9a582c0851ea64d5741fd8f37d5ff29db0
MikeLiam <miguelalcalaromero@gmail.com>
2020-11-12 12:56:41 +0100

    target static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,9 @@
 
 
 export default {
-  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-   mode: 'universal',
 
   // Target (https://go.nuxtjs.dev/config-target)
-  //target: 'static',
+  target: 'static',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
